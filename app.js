cc5d8fedeca947a329ea03f37e0ff03a255a1540
Sunnatillo Azimjonov <monomusix001@gmail.com>
2024-03-28 15:09:11 +0300

    homework 1

---
+++ app.js
@@ -0,0 +1,32 @@
+// 1 masala
+
+// let day = +prompt("son kriting");
+// if(day == 1){
+//     console.log("dushanba");
+// }else if(day == 2){
+//     console.log("seshanba");
+// }else if(day == 3){
+//     console.log("chorshanba");
+// }else if(day == 4){
+//     console.log("payshanba");
+// }else if(day == 5){
+//     console.log("juma");
+// }else if(day == 6){
+//     console.log("shanba");
+// }else if(day == 7){
+//     console.log("yakshanba");
+// }else{
+//     console.log("sonda kunlar yo'q");
+// }
+
+// 2masala
+// do'larda hisobladm
+// let alishPuli = +prompt("how much money have of ALISHER: $ ");
+// let yevro = 120 * 12000; //somda
+// let dollar = Math.floor(yevro / 11000);//dollarga yevroni aylantrdik 
+// let jami = 500 + 250 + dollar;
+// if(alishPuli >= jami){
+//     console.log("oq yol alisher");
+// }else{
+//   console.log("azgina bor hali");
+// }
